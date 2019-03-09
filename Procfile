a9d226b2803240d16c9d361bea998db6e5f01161
huangrh <huangrh@goodrain.com>
2019-03-09 11:07:17 +0800

    [DEL] delete .idea

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar ./webapp-runner.jar --port $PORT java-war-demo-0.0.1.war
+web: java -jar ./webapp-runner.jar --port $PORT java-war-demo.war
--- src/main/java/com/goodrain/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/goodrain/springbootdemo/SpringBootDemoApplication.java
@@ -2,9 +2,10 @@ package com.goodrain.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
